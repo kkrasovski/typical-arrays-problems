c8782b34a38028ef46527ee714280ddf082dfaa6
Kiryl Krasouski <ziksow@gmail.com>
2021-02-25 15:18:53 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length < 1) {
+        return 0;
+    }  else {
+        array.sort((a, b) => a - b);
+        return array[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == undefined || array.length < 1) {
+    return 0;
+} else {
+        array.sort((a, b) => a - b);
+        return array[array.length - 1];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == undefined || array.length < 1) {
+    return 0;
+}  else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let average = sum / array.length;
+        return average;
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
